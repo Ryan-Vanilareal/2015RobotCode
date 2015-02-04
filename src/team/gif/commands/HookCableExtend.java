8661f72601b4da96caed466f02d9992d259f722d
Ryan Villarreal <ryan_villarreal116@yahoo.com>
2015-02-03 18:36:36 -0600

    Uploaded all new commands and subsytems

--- src/team/gif/commands/HookCableExtend.java
+++ src/team/gif/commands/HookCableExtend.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class Hook extends Command {
+public class HookCableExtend extends Command {
 
-    public Hook() {
+    public HookCableExtend() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
